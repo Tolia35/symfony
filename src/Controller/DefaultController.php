4a0fa7d0c0ceb6488a02b83b34f9c2a76e3ce175
pjehan <pierre.jehan@gmail.com>
2019-06-25 11:37:29 +0200

    Affichage de la liste des catégories dans le footer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +18,15 @@ class DefaultController extends AbstractController
 
         return $this->render("default/homepage.html.twig", [
             "articles" => $articles
+        ]);
+    }
+
+    public function navCategory()
+    {
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+
+        return $this->render("default/_navcategory.html.twig", [
+           "categories" => $categories
         ]);
     }
 }
