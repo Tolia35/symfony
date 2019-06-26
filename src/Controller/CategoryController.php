4cf9f34c0f754e98cff4f7dae8db5264a0bc5b36
pjehan <pierre.jehan@gmail.com>
2019-06-26 11:39:28 +0200

    Formulaire de création d'une catégorie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,18 +3,51 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category/{id}", name="category_show")
+     * @Route("/category/{id}", name="category_show", requirements={"id": "\d+"})
      */
     public function show(Category $category)
     {
         return $this->render('category/show.html.twig', [
             'category' => $category
+        ]);
+    }
+
+    /**
+     * @Route("/category/new", name="category_new")
+     */
+    public function new(Request $request)
+    {
+        // Créer un nouvel objet Category
+        $category = new Category();
+
+        // Créer le formulaire pour ajouter une nouvelle Category
+        $form = $this->createForm(CategoryType::class, $category);
+
+        // Mettre à jour le formulaire si celui-ci a été envoyé
+        $form->handleRequest($request);
+
+        // Vérifier si le formulaire a été envoyé et si il est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();// Récupérer les données du formulaire dans l'objet Category
+
+            // Enregister en base de données
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render("category/new.html.twig", [
+            'form' => $form->createView()
         ]);
     }
 }
